9e427091fe04bf063a7ba61717df6f18c6d238c8
navegador <navegador@05000117-070864>
2019-11-13 10:52:25 -0300

    Serialización de arrays.

---
+++ Serializacion/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(160, 200)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 48)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Serializar productos"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(162, 145)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecio.TabIndex = 10
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(159, 128)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Precio"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(160, 102)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(157, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Nombre"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(162, 171)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(439, 346)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Serializacion/Form1.vb
@@ -0,0 +1,71 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Xml.Serialization
+
+Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim p1 As New ProductoBE With {.id = 1, .nombre = "Pera", .precio = 200}
+        Dim p2 As New ProductoBE With {.id = 2, .nombre = "Sandia", .precio = 600}
+        Dim list As New List(Of ProductoBE)
+        list.Add(p1)
+        list.Add(p2)
+
+        Dim xmlSerializer As New XmlSerializer(list.GetType())
+        Dim ms As New MemoryStream
+        xmlSerializer.Serialize(ms, list)
+        Dim result As String = StreamACadena(ms)
+
+        Dim escritor As StreamWriter
+        escritor = File.AppendText("D:\Productos.xml")
+        escritor.Write(result)
+        escritor.Flush()
+        escritor.Close()
+    End Sub
+
+    Public Shared Function StreamACadena(stream As Stream) As String
+
+        Dim bytes(stream.Length) As Byte
+
+        stream.Position = 0
+        stream.Read(bytes, 0, stream.Length)
+
+        Return BytesACadena(bytes)
+
+    End Function
+
+    Public Shared Function BytesACadena(bytes As Byte()) As String
+
+        Return Encoding.ASCII.GetString(bytes)
+
+    End Function
+End Class
+
+Public Class ProductoBE
+    Private _id As Integer
+    Public Property id() As Integer
+        Get
+            Return _id
+        End Get
+        Set(ByVal value As Integer)
+            _id = value
+        End Set
+    End Property
+    Private _nombre As String
+    Public Property nombre() As String
+        Get
+            Return _nombre
+        End Get
+        Set(ByVal value As String)
+            _nombre = value
+        End Set
+    End Property
+    Private _precio As Decimal
+    Public Property precio() As Decimal
+        Get
+            Return _precio
+        End Get
+        Set(ByVal value As Decimal)
+            _precio = value
+        End Set
+    End Property
+End Class
